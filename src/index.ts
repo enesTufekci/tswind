37a5a79acc01ae9cb4ec8df74164f1c7cc45c558
Enes Tufekci <enesxtufekci@gmail.com>
2021-11-11 16:37:50 +0100

    before extend

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-function create() {}
-
-interface Wind {
-  extend: () => void;
-}
-
-const wind = new Proxy(Object.create({}), {
-  get: function (_, prop, __) {
-    if (prop === "") {
-      return "world";
-    }
-    return create;
-  },
-});
+export { wind } from "./wind";
--- src/wind.tsx
+++ src/wind.tsx
@@ -3,47 +3,46 @@ import * as React from "react";
 import { createElement, HTMLAttributes, DetailedHTMLProps } from "react";
 
 type Elements = HTMLElementTagNameMap;
-
-type Input = { [key: string]: { [key: string]: string } };
-
-type Output<T extends Input> = { [key in keyof T]: keyof T[key] };
-
+type Config = { [key: string]: { [key: string]: string } };
+type Props<T extends Config> = { [key in keyof T]: keyof T[key] };
 type Token = string;
 
 interface WindComponent<K extends keyof Elements> {
-  <T extends Input>(
+  <T extends Config>(
     props: DetailedHTMLProps<HTMLAttributes<Elements[K]>, Elements[K]> &
-      Partial<Output<T>>
+      Partial<Props<T>>
   ): JSX.Element;
   computedClasses: string;
 }
 
-interface WindComponentFactory<K extends keyof Elements> {
-  <T extends Input>(input: T | Token, ...tokens: Token[]): WindComponent<K>;
+interface WindComponentFactory<K extends keyof Elements, R extends Config> {
+  <T extends Config>(
+    input: (T & R) | Token,
+    ...tokens: Token[]
+  ): WindComponent<K>;
 }
 
-type Wind = {
-  [K in keyof Elements]: WindComponentFactory<K>;
+type Wind<T extends Config = {}> = {
+  [K in keyof Elements]: WindComponentFactory<K, T>;
 } & {
   extend: <K extends keyof Elements>(
     component: WindComponent<K>
-  ) => WindComponentFactory<K>;
+  ) => Omit<Wind<T>, "extend">;
 };
 
 export const wind: Wind = new Proxy(Object.create({}), {
-  get: function (_, prop: string, __) {
-    if (prop === "a") {
-      return "world";
-    }
-    return create(prop as any);
+  get: function (_, tagName: string, __) {
+    return create(tagName as any);
   },
 });
 
+wind.extend = (() => {}) as any;
+
 function create<T extends keyof Elements>(tagName: T) {
-  return function <K extends Input>(input: K | Token, ...tokens: Token[]) {
+  return function <K extends Config>(input: K | Token, ...tokens: Token[]) {
     console.log(input, tokens, tagName);
     const computedClasses: string[] = [...tokens];
-    return (props: Elements[T] & Partial<Output<K>>) => {
+    return (props: Elements[T] & Partial<Props<K>>) => {
       return createElement(tagName, {
         ...props,
         className: [props.className, ...computedClasses].join(" "),
@@ -68,7 +67,7 @@ export const Rows = wind.div("flex");
 
 const Input = wind.input({ value: { foo: "" } }, "bg-gray-100");
 
-export const BetterInput = wind.extend(Button)("hello");
+export const BetterInput = wind.extend(Button);
 
 export const Test = () => (
   <>
