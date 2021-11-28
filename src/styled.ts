70843e09609a55d94f47858dc8e5d56df0e2651f
Enes Tufekci <enesxtufekci@gmail.com>
2021-11-28 22:15:39 +0100

    fix extend type

--- src/styled.ts
+++ src/styled.ts
@@ -2,20 +2,22 @@ import { createComponent, WindComponent } from "./createComponent";
 import { normalizeConfig } from "./normalizeConfig";
 import { Elements, Flags, Token } from "./types";
 
-type StyledFn<K extends Elements> = <T extends Flags | Token | undefined>(
+type StyledFn<T0 extends Flags, K extends Elements> = <
+  T extends Flags | Token | undefined
+>(
   flags?: T,
   ...tokens: Token[]
-) => WindComponent<T extends Flags ? T : {}, K>;
+) => WindComponent<T extends Flags ? T0 & T : T0, K>;
 
-export type Styled = {
-  [K in keyof JSX.IntrinsicElements]: StyledFn<K>;
+export type Styled<T extends Flags> = {
+  [K in keyof JSX.IntrinsicElements]: StyledFn<T, K>;
 };
 
 export function createStyled<T extends Flags>(
-  __flags?: T | Token,
+  __flags: T,
   ...__tokens: Token[]
-) {
-  return new Proxy({} as Styled, {
+): Styled<T> {
+  return new Proxy({} as Styled<T>, {
     get: (_, element: Elements) => {
       return (_flags: Flags, ..._tokens: Token[]) => {
         const { flags, tokens } = normalizeConfig(
@@ -35,4 +37,4 @@ export function createStyled<T extends Flags>(
   });
 }
 
-export const styled = createStyled();
+export const styled = createStyled({});
